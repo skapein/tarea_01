3268d8dd1b77802c05f84a7a733649e0e36c4c7b
chrisMJ <chris666.10@hotmail.com>
2024-08-01 17:09:24 -0500

    mostrar variables

--- tarea.php
+++ tarea.php
@@ -6,3 +6,10 @@ $stringVar = "Hola Mundo";
 $booleanVar = true;        
 $arrayVar = array("manzana", "banana", "cereza"); // Variable de array
 
+// Mostrar las variables
+echo "Variable entera: " . $integerVar . "<br>";
+echo "Variable de punto flotante: " . $floatVar . "<br>";
+echo "Variable de cadena: " . $stringVar . "<br>";
+echo "Variable booleana: " . ($booleanVar ? 'true' : 'false') . "<br>";
+echo "Variable de array: " . implode(", ", $arrayVar) . "<br>";
+?>
