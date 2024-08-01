0cc611216616a224f480c228d523f9a016deab08
chrisMJ <chris666.10@hotmail.com>
2024-08-01 17:13:08 -0500

    Declarar variables y operaciones artimeticas

--- tarea.php
+++ tarea.php
@@ -12,13 +12,13 @@ echo "Variable de punto flotante: " . $floatVar . "<br>";
 echo "Variable de cadena: " . $stringVar . "<br>";
 echo "Variable booleana: " . ($booleanVar ? 'true' : 'false') . "<br>";
 echo "Variable de array: " . implode(", ", $arrayVar) . "<br>";
-?>
+
 
 // Operaciones Aritméticas
 $suma = $integerVar + $floatVar;       // Suma
 $multiplicacion = $integerVar * $floatVar; // Multiplicación
 
-// Mostrar resultados de operaciones aritméticas
+// Mostrar resultados de operaciones aritméticas usando echo
 echo "Resultado de la suma: " . $suma . "<br>";
 echo "Resultado de la multiplicación: " . $multiplicacion . "<br>";
 ?>
